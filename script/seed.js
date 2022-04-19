c7401233348cfa22864613c5e296781b7eb7b8d5
Eduardo Turcios <eddieturcios@outlook.com>
2022-04-19 11:26:02 -0400

    seed: added products to seed

--- script/seed.js
+++ script/seed.js
@@ -1,29 +1,64 @@
-'use strict'
+'use strict';
 
-const {db, models: {User} } = require('../server/db')
+const {
+  db,
+  models: { User, Product },
+} = require('../server/db');
 
 /**
  * seed - this function clears the database, updates tables to
  *      match the models, and populates the database.
  */
 async function seed() {
-  await db.sync({ force: true }) // clears db and matches models to tables
-  console.log('db synced!')
+  await db.sync({ force: true }); // clears db and matches models to tables
+  console.log('db synced!');
 
   // Creating Users
   const users = await Promise.all([
     User.create({ username: 'cody', password: '123' }),
     User.create({ username: 'murphy', password: '123' }),
-  ])
+  ]);
+  // name: unknown;
+  // description: unknown;
+  // price: unknown;
+  // stock: unknown;
+  // imageUrl: unknown;
+  const products = await Promise.all([
+    Product.create({
+      name: 'Liver',
+      description:
+        'The liver is a major organ only found in vertebrates which performs many essential biological functions such as detoxification of the organism, and the synthesis of proteins and biochemicals necessary for digestion and growth.',
+      price: 10000.0,
+      stock: 20,
+      imageUrl: 'https://www.lifespan.io/wp-content/uploads/2020/11/liver.jpg',
+    }),
+    Product.create({
+      name: 'mystery box',
+      description: 'What is it?',
+      price: 20000.0,
+      stock: 20,
+      imageUrl:
+        'https://powelllacrosse.com/wp-content/uploads/2020/04/mysterybox.jpg',
+    }),
+    Product.create({
+      name: 'AK-47',
+      description:
+        'The AK-47, officially known as the Avtomat Kalashnikov, is a gas-operated assault rifle that is chambered for the 7.62 x 39mm cartridge',
+      price: 3000.0,
+      stock: 20,
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/6/65/AK-47_type_II_noBG.png',
+    }),
+  ]);
 
-  console.log(`seeded ${users.length} users`)
-  console.log(`seeded successfully`)
+  console.log(`seeded ${users.length} users`);
+  console.log(`seeded successfully`);
   return {
     users: {
       cody: users[0],
-      murphy: users[1]
-    }
-  }
+      murphy: users[1],
+    },
+  };
 }
 
 /*
@@ -32,16 +67,16 @@ async function seed() {
  The `seed` function is concerned only with modifying the database.
 */
 async function runSeed() {
-  console.log('seeding...')
+  console.log('seeding...');
   try {
-    await seed()
+    await seed();
   } catch (err) {
-    console.error(err)
-    process.exitCode = 1
+    console.error(err);
+    process.exitCode = 1;
   } finally {
-    console.log('closing db connection')
-    await db.close()
-    console.log('db connection closed')
+    console.log('closing db connection');
+    await db.close();
+    console.log('db connection closed');
   }
 }
 
@@ -51,8 +86,8 @@ async function runSeed() {
   any errors that might occur inside of `seed`.
 */
 if (module === require.main) {
-  runSeed()
+  runSeed();
 }
 
 // we export the seed function for testing purposes (see `./seed.spec.js`)
-module.exports = seed
+module.exports = seed;
