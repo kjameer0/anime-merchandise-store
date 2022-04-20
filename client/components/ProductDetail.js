3b23ae1cd752bee71dddbb55156b18b90c5237d8
Kyle Gilbert <gilbertkyle@yahoo.com>
2022-04-20 07:31:57 -0700

    fixed product detail view and reducer

--- client/components/ProductDetail.js
+++ client/components/ProductDetail.js
@@ -12,23 +12,23 @@ export class ProductDetail extends Component {
       product: { name, description, price, stock, imageUrl },
     } = this.props;
     return (
-      <div>
+      <div className="product-detail">
         <h1> name: {name}</h1>
         <img src={imageUrl} />
-        <h2> description: {description}</h2>
-        <h2> price: {price}</h2>
-        <h2> stock: {stock}</h2>
+        <p> description: {description}</p>
+        <p> price: {price}</p>
+        <p> stock: {stock}</p>
       </div>
     );
   }
 }
 
-const mapState = (state) => ({
+const mapState = state => ({
   product: state.product,
 });
 
-const mapDispatch = (dispatch) => ({
-  setSingleProductThunk: (id) => dispatch(setSingleProductThunk(id)),
+const mapDispatch = dispatch => ({
+  setSingleProductThunk: id => dispatch(setSingleProductThunk(id)),
 });
 
 export default connect(mapState, mapDispatch)(ProductDetail);
--- client/store/index.js
+++ client/store/index.js
@@ -1,15 +1,13 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux'
-import {createLogger} from 'redux-logger'
-import thunkMiddleware from 'redux-thunk'
-import {composeWithDevTools} from 'redux-devtools-extension'
-import auth from './auth'
-import allProductsReducer from './allProducts'
-import singleProductReducer from './singleProduct'
-const reducer = combineReducers({ auth, products: allProductsReducer, singleProductReducer })
-const middleware = composeWithDevTools(
-  applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
-)
-const store = createStore(reducer, middleware)
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createLogger } from "redux-logger";
+import thunkMiddleware from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import auth from "./auth";
+import allProductsReducer from "./allProducts";
+import singleProductReducer from "./singleProduct";
+const reducer = combineReducers({ auth, products: allProductsReducer, product: singleProductReducer });
+const middleware = composeWithDevTools(applyMiddleware(thunkMiddleware, createLogger({ collapsed: true })));
+const store = createStore(reducer, middleware);
 
-export default store
-export * from './auth'
+export default store;
+export * from "./auth";
