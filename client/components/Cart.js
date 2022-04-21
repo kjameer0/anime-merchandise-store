592f6211928f6741bd7b71b5fafc994c782fb739
Eduardo Turcios <eddieturcios@outlook.com>
2022-04-21 14:49:57 -0400

    Feat: added cart state and buttons for routes

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -1,15 +1,54 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { setCartThunk } from '../store/cart';
+import SelectQuantity from './SelectQuantity';
+import { clearCart } from '../store/cart';
 export class Cart extends Component {
   constructor(props) {
     super(props);
+    this.state = { cart: [] };
     this.handleOnChange = this.handleOnChange.bind(this);
+    this.handleCheckout = this.handleCheckout.bind(this);
+    this.handleSingleRemove = this.handleSingleRemove.bind(this);
   }
 
-  handleOnChange = (event) => {};
+  componentDidUpdate(prevProps) {
+    if (!prevProps.cart.length && this.props.cart.length) {
+      const newCart = [];
+      this.props.cart.map((cart) => {
+        newCart.push({
+          productId: cart.product.id,
+          quantity: cart.quantity,
+          price: cart.product.price,
+        });
+      });
+      this.setState({
+        cart: [...this.state.cart, ...newCart],
+      });
+    }
+  }
+  componentWillUnmount() {
+    this.props.clearCart();
+  }
   componentDidMount() {
     this.props.fetchCart();
+  }
+  handleOnChange(event, index) {
+    const productId = this.state.cart[index].productId;
+    const newCart = this.state.cart.map((cart) => {
+      return cart.productId !== productId
+        ? cart
+        : {
+            productId: productId,
+            quantity: Number(event.target.value),
+            price: cart.price,
+          };
+    });
+    this.setState({ cart: newCart });
+  }
+  handleSingleRemove(index) {}
+  handleCheckout(event) {
+    event.preventDefault();
   }
   render() {
     const userCart = this.props.cart;
@@ -20,41 +59,42 @@ export class Cart extends Component {
         </div>
       );
     return (
-      <div>
+      <form id="form-cart" onSubmit={this.handleCheckout}>
         <div className="all-cart">
-          {userCart.map((cart) => (
+          {userCart.map((cart, index) => (
             <div className="cart" key={cart.id}>
               <div className="cart-data">
                 <img src={cart.product.imageUrl} alt={cart.product.name} />
                 <div className="cart-info">
                   <p className="product-name">{cart.product.name}</p>
                   <p className="cart-price">${cart.product.price}</p>
-                  <CartOptions
+                  <SelectQuantity
                     quantity={cart.quantity}
-                    onChange={this.handleOnChange}
+                    onChange={(event) => this.handleOnChange(event, index)}
                   />
                 </div>
               </div>
               <div className="cart-delete">
-                <button type="button">remove from cart</button>
+                <button type="button" onClick={() => handleSingleRemove(index)}>
+                  remove from cart
+                </button>
               </div>
             </div>
           ))}
         </div>
 
-        <SubTotal userCart={userCart} />
-      </div>
+        <SubTotal items={this.state.cart} />
+
+        <button type="button">checkout</button>
+      </form>
     );
   }
 }
 
 const SubTotal = (props) => {
-  const totalItem = props.userCart.reduce(
-    (sum, item) => sum + item.quantity,
-    0
-  );
-  const totalCost = props.userCart.reduce(
-    (sum, item) => sum + item.quantity * item.product.price,
+  const totalItem = props.items.reduce((sum, item) => sum + item.quantity, 0);
+  const totalCost = props.items.reduce(
+    (sum, item) => sum + item.quantity * item.price,
     0
   );
   return (
@@ -66,27 +106,14 @@ const SubTotal = (props) => {
   );
 };
 
-const CartOptions = (props) => {
-  const quantity = Array.from({ length: props.quantity }, (_, i) => i + 1);
-  return (
-    <label>
-      quantity:
-      <select defaultValue={props.quantity} onChange={props.onChange}>
-        {quantity.map((number) => (
-          <option value={number} key={number}>
-            {number}
-          </option>
-        ))}
-      </select>
-    </label>
-  );
-};
-
 const mapState = (state) => {
   return { cart: state.cart };
 };
 const mapDispatch = (dispatch) => {
-  return { fetchCart: () => dispatch(setCartThunk()) };
+  return {
+    fetchCart: () => dispatch(setCartThunk()),
+    clearCart: () => dispatch(clearCart()),
+  };
 };
 
 export default connect(mapState, mapDispatch)(Cart);
