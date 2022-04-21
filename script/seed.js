eef6711cdff5930ec7f035e217bc4c2c68818c72
Masaki Shibano <masakishibano@gmail.com>
2022-04-21 15:04:49 -0700

    fixed valo weapons

--- script/seed.js
+++ script/seed.js
@@ -3,7 +3,7 @@ const {
   db,
   models: { User, Product, CartItem, Order },
 } = require("../server/db");
-const { faker } = require("@faker-js/faker"); 
+const { faker } = require("@faker-js/faker");
 /**
  * seed - this function clears the database, updates tables to
  *      match the models, and populates the database.
@@ -15,11 +15,16 @@ async function seed() {
 
   // Creating Users
   const users = await Promise.all([
-
-
-    User.create({ username: "cody", password: "123", email: 'cody123@gmail.com' }),
-    User.create({ username: "murphy", password: "123", email: 'murphy123@yahoo.com' }),
-
+    User.create({
+      username: "cody",
+      password: "123",
+      email: "cody123@gmail.com",
+    }),
+    User.create({
+      username: "murphy",
+      password: "123",
+      email: "murphy123@yahoo.com",
+    }),
   ]);
 
   // name: unknown;
@@ -41,7 +46,8 @@ async function seed() {
       description: "What is it?",
       price: 20000.0,
       stock: 20,
-      imageUrl: "https://powelllacrosse.com/wp-content/uploads/2020/04/mysterybox.jpg",
+      imageUrl:
+        "https://powelllacrosse.com/wp-content/uploads/2020/04/mysterybox.jpg",
     }),
     Product.create({
       name: "Weber Traveler Portable Propane Gas Grill - Black - 9010001",
@@ -58,7 +64,8 @@ async function seed() {
         "Heart Containers, also known as Bowls of Hearts, Heart-Shaped Stones, Life Hearts, and Crystal Hearts, are recurring Items in The Legend of Zelda series. They increase Link's Life Gauge, which is represented by a set of Hearts, excluding The Adventure of Link, where his health is symbolized by a life bar. Collecting enough Pieces of Heart assembles a Heart Container and increases your maximum health.",
       price: 1200.0,
       stock: 30,
-      imageUrl: "https://static.wikia.nocookie.net/zelda_gamepedia_en/images/0/0f/LANS_Heart_Container_Model.png",
+      imageUrl:
+        "https://static.wikia.nocookie.net/zelda_gamepedia_en/images/0/0f/LANS_Heart_Container_Model.png",
     }),
     Product.create({
       name: "Keyblades",
@@ -74,7 +81,8 @@ async function seed() {
         "The Pandora Box (パンドラボックス, Pandora Bokkusu) alternatively spelt Pandora's Box, is an extraterrestrial artifact which was used by Evolto and Blood Tribe to destroy numerous planets across the universe.",
       price: 75000.0,
       stock: 4,
-      imageUrl: "https://static.wikia.nocookie.net/kamenrider/images/6/6e/KRBu-Pandora_Box.png",
+      imageUrl:
+        "https://static.wikia.nocookie.net/kamenrider/images/6/6e/KRBu-Pandora_Box.png",
     }),
     Product.create({
       name: "Black hole grenades",
@@ -82,7 +90,8 @@ async function seed() {
         "Black hole grenades are small dark egg-shaped items that glow red when about to detonate. Upon detonation, they suck everything within range into themselves, creating a several-second gravitational singularity that seemingly wipes them from existence. The range appears to be approximately two feet, as those who are close by are still not affected unless they are nearly touching one another.",
       price: 123456789.0,
       stock: 5,
-      imageUrl: "https://static.wikia.nocookie.net/callofduty/images/5/50/LT53_Promo_BOCW.jpg",
+      imageUrl:
+        "https://static.wikia.nocookie.net/callofduty/images/5/50/LT53_Promo_BOCW.jpg",
     }),
     Product.create({
       name: "Ultima Weapon",
@@ -90,7 +99,8 @@ async function seed() {
         "The Ultima Weapon (アルテマウェポン, Arutema Wepon?), also known as Atma Weapon or Caladbolg, is a weapon in many games in the Final Fantasy series. Like the spell, the Ultima Weapon is one of the strongest in the game and often serves as the ultimate weapon of the main protagonist.",
       price: 9999.99,
       stock: 7,
-      imageUrl: "https://static.wikia.nocookie.net/finalfantasy/images/5/58/Dissidia-UltimaWeapon.png",
+      imageUrl:
+        "https://static.wikia.nocookie.net/finalfantasy/images/5/58/Dissidia-UltimaWeapon.png",
     }),
     Product.create({
       name: "Chaos Emeralds",
@@ -98,7 +108,8 @@ async function seed() {
         "They are seven ancient emeralds and mystical relics tied to the Master Emerald that possess powerful properties and abilities. Those that hold the Chaos Emeralds can use them for a variety of things, such as warping time and space, powering machines, and initiating super transformations. Anyone who combines all seven Chaos Emeralds can control unimaginable power.",
       price: 8530000.0,
       stock: 7,
-      imageUrl: "https://static.wikia.nocookie.net/sonic/images/7/79/Treasure_Hunter.png",
+      imageUrl:
+        "https://static.wikia.nocookie.net/sonic/images/7/79/Treasure_Hunter.png",
     }),
     Product.create({
       name: "Lightsaber",
@@ -106,11 +117,12 @@ async function seed() {
         "The lightsaber, also referred to as a laser sword by those who were unfamiliar with it, was a weapon usually used by the Jedi, the Sith, and other Force-sensitives. Lightsabers consisted of a plasma blade, powered by a kyber crystal, that was emitted from a usually metal hilt and could be shut off at will.",
       price: 66000.0,
       stock: 66,
-      imageUrl: "https://static.wikia.nocookie.net/fortnite_gamepedia/images/b/b7/Rey_Lightsaber.png",
+      imageUrl:
+        "https://static.wikia.nocookie.net/fortnite_gamepedia/images/b/b7/Rey_Lightsaber.png",
     }),
     Product.create({
       name: "Gunblade",
-      description: "What is it?",
+      description: "Gun + Blade, what else is there to say?",
       price: 20000.0,
       stock: 20,
       imageUrl:
@@ -123,7 +135,7 @@ async function seed() {
       price: 20000.0,
       stock: 20,
       imageUrl:
-        "https://static.wikia.nocookie.net/valorant/images/5/56/Vandal.png",
+        "https://cdn1.dotesports.com/wp-content/uploads/2021/04/30135716/image-45-2048x948.png",
     }),
     Product.create({
       name: "Phantom",
@@ -132,7 +144,7 @@ async function seed() {
       price: 20000.0,
       stock: 20,
       imageUrl:
-        "https://static.wikia.nocookie.net/valorant/images/e/ec/Phantom.png",
+        "https://cdn1.dotesports.com/wp-content/uploads/2021/05/07110811/image-4-2048x978.png",
     }),
     Product.create({
       name: "Golden Gun",
