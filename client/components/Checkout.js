b94a75ad81c8bb7f30bb02c5ae0ab9fe26e215b8
Eduardo Turcios <eddieturcios@outlook.com>
2022-04-25 16:40:09 -0400

    added createOrder

--- client/components/Checkout.js
+++ client/components/Checkout.js
@@ -6,17 +6,57 @@ import { clearCart } from '../store/cart';
 import { orderCheckoutThunk } from '../store/singleOrder';
 import SingleOrderItem from './SingleOrderItem';
 import SubTotal from './SubTotal';
+
 const CLIENT_ID =
   'AcA34C5GAOSbQO0YP7JuUdhfyzk0BlTYIt-y2UaTwolNq5NAddiOOcqKCUQHYLM6vW_KIYYJD1QLBBjj';
 
 export class Checkout extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { totalPrice: 0 };
+    this.createOrder = this.createOrder.bind(this);
+  }
   componentWillUnmount() {
     this.props.clearCart();
   }
   componentDidMount() {
     this.props.fetchCart();
   }
-
+  createOrder(data, actions) {
+    const { email, firstName, lastName, address } = this.props;
+    return actions.order.create({
+      intent: 'CAPTURE',
+      payer: {
+        name: {
+          given_name: firstName,
+          surname: lastName,
+        },
+        address: {
+          address_line_1: '1231 Broadway',
+          address_line_2: 'APT 2R',
+          admin_area_2: 'Brooklyn',
+          admin_area_1: 'NY',
+          postal_code: '11206',
+          country_code: 'US',
+        },
+        email_address: email,
+      },
+      purchase_units: [
+        {
+          amount: {
+            currency_code: 'USD',
+            value: '10.00',
+          },
+        },
+      ],
+    });
+  }
+  onApprove(data, actions) {
+    return actions.order.capture().then((details) => {
+      console.log(details);
+      alert(`Transaction completed by ${name}`);
+    });
+  }
   render() {
     return (
       <div className="checkout">
@@ -27,7 +67,11 @@ export class Checkout extends Component {
             <p>Expexcted deliver date April 29, 2022</p>
           </div>
           <PayPalScriptProvider options={{ 'client-id': CLIENT_ID }}>
-            <PayPalButtons style={{ layout: 'vertical' }} />
+            <PayPalButtons
+              style={{ layout: 'vertical' }}
+              createOrder={this.createOrder}
+              onApprove={this.onApprove}
+            />
           </PayPalScriptProvider>
         </div>
         <div id="order-summary">
@@ -44,7 +88,13 @@ export class Checkout extends Component {
   }
 }
 
-const mapStateToProps = (state) => ({ cart: state.cart });
+const mapStateToProps = (state) => ({
+  cart: state.cart,
+  email: state.auth.email,
+  firstName: state.auth.firstName,
+  lastName: state.auth.lastName,
+  address: state.auth.address,
+});
 
 const mapDispatchToProps = (dispatch) => {
   return {
