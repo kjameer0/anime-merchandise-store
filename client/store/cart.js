68b760206afac260108f50e08da1cb75f2b698b4
Eduardo Turcios <eddieturcios@outlook.com>
2022-04-26 10:57:35 -0400

    bug: fixed setCart Thunk

--- client/store/cart.js
+++ client/store/cart.js
@@ -1,49 +1,50 @@
-import axios from "axios";
-import history from "../history";
+import axios from 'axios';
+import history from '../history';
 
-const SET_CART = "SET_CART";
-const ADD_CART = "ADD_CART";
-const DELETE_ITEM = "DELETE_ITEM";
-const UPDATE_CART = "UPDATE_CART";
-const CLEAR_CART = "CLEAR_CART";
-const setCart = cart => ({ type: SET_CART, cart });
-const addToCart = product => ({ type: ADD_CART, product });
-const updateCart = newCart => ({ type: UPDATE_CART, newCart });
-const deleteFromCart = product => ({ type: DELETE_ITEM, product });
+const SET_CART = 'SET_CART';
+const ADD_CART = 'ADD_CART';
+const DELETE_ITEM = 'DELETE_ITEM';
+const UPDATE_CART = 'UPDATE_CART';
+const CLEAR_CART = 'CLEAR_CART';
+const setCart = (cart) => ({ type: SET_CART, cart });
+const addToCart = (product) => ({ type: ADD_CART, product });
+const updateCart = (newCart) => ({ type: UPDATE_CART, newCart });
+const deleteFromCart = (product) => ({ type: DELETE_ITEM, product });
 
 export const clearCart = () => ({ type: CLEAR_CART, cart: [] });
 
 export const setCartThunk = () => {
-  return async dispatch => {
+  return async (dispatch) => {
     try {
-      const token = window.localStorage.getItem("token");
+      const token = window.localStorage.getItem('token');
+      const cart = window.localStorage.getItem('cart');
       if (token) {
-        if (JSON.parse(window.localStorage.getItem("cart")).length) {
-          let storageCart = JSON.parse(window.localStorage.getItem("cart"));
+        if (cart && JSON.parse(cart).length) {
+          let storageCart = JSON.parse(window.localStorage.getItem('cart'));
           for (let i = 0; i < storageCart.length; i++) {
-            await axios.post("/api/cart/", storageCart[i], {
+            await axios.post('/api/cart/', storageCart[i], {
               headers: {
                 authorization: token,
               },
             });
           }
         }
-        const { data } = await axios.get("/api/cart", {
+        const { data } = await axios.get('/api/cart', {
           headers: {
             authorization: token,
           },
         });
         console.log(data);
         dispatch(setCart(data));
-        window.localStorage.setItem("cart", JSON.stringify([]));
+        window.localStorage.setItem('cart', JSON.stringify([]));
       } else {
-        if (JSON.parse(window.localStorage.getItem("cart")).length) {
-          dispatch(setCart(JSON.parse(window.localStorage.getItem("cart"))));
+        if (JSON.parse(window.localStorage.getItem('cart')).length) {
+          dispatch(setCart(JSON.parse(window.localStorage.getItem('cart'))));
         } else {
-          if (JSON.parse(window.localStorage.getItem("cart")).length) {
-            dispatch(setCart(JSON.parse(window.localStorage.getItem("cart"))));
+          if (JSON.parse(window.localStorage.getItem('cart')).length) {
+            dispatch(setCart(JSON.parse(window.localStorage.getItem('cart'))));
           } else {
-            window.localStorage.setItem("cart", JSON.stringify([]));
+            window.localStorage.setItem('cart', JSON.stringify([]));
             dispatch(setCart([]));
           }
         }
@@ -54,26 +55,26 @@ export const setCartThunk = () => {
   };
 };
 
-export const deleteFromCartThunk = productInfo => {
-  return async dispatch => {
+export const deleteFromCartThunk = (productInfo) => {
+  return async (dispatch) => {
     try {
-      const token = window.localStorage.getItem("token") || "";
+      const token = window.localStorage.getItem('token') || '';
       if (token) {
-        const { data } = await axios.delete("/api/cart/", {
+        const { data } = await axios.delete('/api/cart/', {
           headers: {
             authorization: token,
           },
           data: { id: productInfo.id },
         });
-        console.log("loggedid", data);
+        console.log('loggedid', data);
         dispatch(deleteFromCart(data));
       } else {
-        let currentCart = JSON.parse(window.localStorage.getItem("cart"));
+        let currentCart = JSON.parse(window.localStorage.getItem('cart'));
         currentCart = currentCart.filter(
           (element) => element.id !== productInfo.id
         );
-        window.localStorage.setItem("cart", JSON.stringify(currentCart));
-        console.log("loggedout", productInfo);
+        window.localStorage.setItem('cart', JSON.stringify(currentCart));
+        console.log('loggedout', productInfo);
         dispatch(deleteFromCart(productInfo));
       }
     } catch (error) {
@@ -82,19 +83,19 @@ export const deleteFromCartThunk = productInfo => {
   };
 };
 
-export const addToCartThunk = productInfo => {
-  return async dispatch => {
+export const addToCartThunk = (productInfo) => {
+  return async (dispatch) => {
     try {
-      const token = window.localStorage.getItem("token") || "";
+      const token = window.localStorage.getItem('token') || '';
       if (token) {
-        const { data } = await axios.post("/api/cart", productInfo, {
+        const { data } = await axios.post('/api/cart', productInfo, {
           headers: {
             authorization: token,
           },
         });
         dispatch(addToCart(data));
       } else {
-        let localCart = JSON.parse(window.localStorage.getItem("cart"));
+        let localCart = JSON.parse(window.localStorage.getItem('cart'));
         let newCart;
         let cartItem;
         if (localCart.some((item) => item.product.id === productInfo.id)) {
@@ -115,7 +116,7 @@ export const addToCartThunk = productInfo => {
           newCart = localCart;
         }
         localCart = window.localStorage.setItem(
-          "cart",
+          'cart',
           JSON.stringify(newCart)
         );
         dispatch(addToCart(cartItem));
@@ -126,19 +127,19 @@ export const addToCartThunk = productInfo => {
   };
 };
 //
-export const updateCartThunk = productInfo => {
-  return async dispatch => {
+export const updateCartThunk = (productInfo) => {
+  return async (dispatch) => {
     try {
-      const token = window.localStorage.getItem("token") || "";
+      const token = window.localStorage.getItem('token') || '';
       if (token) {
-        const { data } = await axios.put("/api/cart/", productInfo, {
+        const { data } = await axios.put('/api/cart/', productInfo, {
           headers: {
             authorization: token,
           },
         });
         dispatch(updateCart(data));
       } else {
-        let currentCart = JSON.parse(window.localStorage.getItem("cart"));
+        let currentCart = JSON.parse(window.localStorage.getItem('cart'));
         let newCart = currentCart.map((item) => {
           if (item.id === productInfo.id) {
             item.quantity = productInfo.quantity;
@@ -146,7 +147,7 @@ export const updateCartThunk = productInfo => {
           return item;
         });
         currentCart.push(productInfo);
-        window.localStorage.setItem("cart", JSON.stringify(currentCart));
+        window.localStorage.setItem('cart', JSON.stringify(currentCart));
         dispatch(updateCart(productInfo));
       }
     } catch (error) {
@@ -172,9 +173,9 @@ export default function cartReducer(state = initialState, action) {
         return [...state, action.product];
       }
     case DELETE_ITEM:
-      return state.filter(item => item.id !== action.product.id);
+      return state.filter((item) => item.id !== action.product.id);
     case UPDATE_CART:
-      return state.map(item => {
+      return state.map((item) => {
         if (item.id === action.newCart.id) return action.newCart;
         return item;
       });
