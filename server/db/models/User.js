3988827a7961dcdd32d67f67196c11274350c00f
Kyle Gilbert <gilbertkyle@yahoo.com>
2022-04-26 08:07:44 -0700

    removed dead code

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -2,7 +2,6 @@ const Sequelize = require("sequelize");
 const db = require("../db");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
-const axios = require("axios");
 
 const SALT_ROUNDS = 5;
 
@@ -89,7 +88,7 @@ User.findByToken = async function (token) {
 /**
  * hooks
  */
-const hashPassword = async (user) => {
+const hashPassword = async user => {
   //in case the password has been changed, we want to encrypt it with bcrypt
   if (user.changed("password")) {
     user.password = await bcrypt.hash(user.password, SALT_ROUNDS);
@@ -98,4 +97,4 @@ const hashPassword = async (user) => {
 
 User.beforeCreate(hashPassword);
 User.beforeUpdate(hashPassword);
-User.beforeBulkCreate((users) => Promise.all(users.map(hashPassword)));
+User.beforeBulkCreate(users => Promise.all(users.map(hashPassword)));
